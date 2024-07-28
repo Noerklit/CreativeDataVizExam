6e1a2cb378bc30b8cf9209b3c4ded33a0dd8ed91
Mads Nørklit Jensen <mchrnj@gmail.com>
2024-07-29 01:04:32 +0200

    Loading csv data in, starting to sort through it

--- Exam_project/index2.js
+++ Exam_project/index2.js
@@ -8,9 +8,6 @@ var svg = d3.select("#canvas").append("svg")
 
 
 // // Define a projection (e.g., Mercator) and a path generator
-
-
-
 // const projection = d3.geoOrthographic()
 //     .scale(300) // Adjust the scale for the globe
 //     .translate([width / 2, height / 2])
@@ -22,31 +19,49 @@ const projection = d3.geoEquirectangular()
 const path = d3.geoPath()
     .projection(projection);
 
+var indianOcean = {};
+var northPacificOcean = {};
+var southPacificOcean = {}; // Just called Pacific Ocean in data
+var northAtlanticOcean = {};
+var southAtlanticOcean = {}; //Just called Atlantic Ocean in data
+var southernOcean = {};
+var arcticOcean = {};
+
 
 // Define a color scale
-const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-
+// const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+const colorScale = d3.scaleSequential(d3.interpolateBlues);
 
     // Load GeoJSON data
 d3.json("oceans-topo-simple.json").then(function(data) {
     console.log(data)
     var subunits = topojson.feature(data, data.objects.oceans)
 
-    // Bind the data and create one path per feature
-    svg.selectAll("path")
-        .data(subunits.features)
-        .enter().append("path")
-        .attr("class", function(d) { return "subunit " + d.id; })
-        .attr("d", path)
-        .attr("fill", (d, i) => colorScale(i)) // Fill each polygon with a different color
-        .attr("fill-opacity", 0.2) // Set the opacity
-        .attr("stroke", "black")
-        .attr("stroke-width", 0.9)
-        .attr("id", (d, i) => "polygon-" + i) // Assign a unique ID
-        .attr("class", (d, i) => "polygon-class-" + i); // Optionally assign a unique class
+    d3.csv("data.csv").then(function(csvData) {
+        const colorMapping = {};
+        csvData.forEach(function(d) {
+            const oceanName = d.Measure;
+            const dateOfMeasurement = d.Date;
+            const value = +d.Value;
 
-    svg.selectAll("path").each(function(d, i) {
-      console.log(d.id, this.id, this.getAttribute('class'));
+            colorMapping[d.id] = d.color;
+        });
+        // Bind the data and create one path per feature
+        svg.selectAll("path")
+            .data(subunits.features)
+            .enter().append("path")
+            .attr("class", function(d) { return "subunit " + d.id; })
+            .attr("d", path)
+            .attr("fill", function(d) { return colorMapping[d.id] || colorScale(Math.random()); }) // Fill each polygon with a different color
+            .attr("fill-opacity", 0.2) // Set the opacity
+            .attr("stroke", "black")
+            .attr("stroke-width", 0.9)
+            .attr("id", (d, i) => "polygon-" + i) // Assign a unique ID
+            .attr("class", (d, i) => "polygon-class-" + i); // Optionally assign a unique class
+
+        svg.selectAll("path").each(function(d, i) {
+            console.log(d.id, this.id, this.getAttribute('class'));
+        });
     });
 
   //   // Optionally, add rotation to the globe
